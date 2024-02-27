a2dec222b16987e7dd1c1a476365d9adb14e46d0
Taeyoung Won <hexter1994@gmail.com>
2024-02-28 00:21:21 +0900

    [Fix] 트렌드코인, 코인 모델 병합, 미사용 DTO 삭제

--- KazCoin/Data/Model/DTO/TrendDTO.swift
+++ KazCoin/Data/Model/DTO/TrendDTO.swift
@@ -9,13 +9,13 @@ import KazUtility
 
 struct TrendResponseDTO: DTO {
   
-  let trendCoins: [TrendCoinDTO] // 트렌드 코인
-  let nfts: [NFTDTO]             // NFT
+  let coins: [TrendCoinDTO] // 트렌드 코인
+  let nfts: [NFTDTO]        // NFT
   
-  func toEntity() -> TrendResponse {
+  func toEntity() -> Trend {
     
-    return TrendResponse(
-      trendCoins: trendCoins.map { $0.toEntity() },
+    return Trend(
+      coins: coins.map { $0.item.toEntity() },
       nfts: nfts.map { $0.toEntity() }
     )
   }
@@ -23,31 +23,21 @@ struct TrendResponseDTO: DTO {
   static var defaultValue: TrendResponseDTO {
     
     return TrendResponseDTO(
-      trendCoins: [.defaultValue],
+      coins: [.defaultValue],
       nfts: [.defaultValue]
     )
   }
 }
 
 struct TrendCoinDTO: DTO {
-    
-  let item: TrendCoinItemDTO // 코인 정보
+  
+  let item: CoinDTO
   
   func toEntity() -> TrendCoin {
-    
-    return TrendCoin(
-      id: item.id,
-      name: item.name,
-      symbol: item.symbol,
-      icon: item.small,
-      marketRank: item.market_cap_rank,
-      price: item.data.price,
-      priceChangeRateKRW: item.data.price_change_percentage_24h.krw
-    )
+    return TrendCoin(item: item.toEntity())
   }
   
   static var defaultValue: TrendCoinDTO {
-    
     return TrendCoinDTO(item: .defaultValue)
   }
 }
@@ -86,49 +76,12 @@ struct NFTDTO: DTO {
   }
 }
 
-
-struct TrendCoinItemDTO: DefaultValueProvidable {
-  
-  let id: String              // 코인 ID
-  let name: String            // 코인 이름
-  let symbol: String          // 코인 식별자 심볼
-  let market_cap_rank: Int    // 코인 랭킹
-  let small: String           // 코인 아이콘 리소스 URL
-  let data: TrendCoinPriceDTO // 코인 가격 정보
-  
-  static var defaultValue: TrendCoinItemDTO {
-    
-    return TrendCoinItemDTO(
-      id: .defaultValue,
-      name: .defaultValue,
-      symbol: .defaultValue,
-      market_cap_rank: .defaultValue,
-      small: .defaultValue,
-      data: .defaultValue
-    )
-  }
-}
-
 struct TrendCoinPriceDTO: DefaultValueProvidable {
   
   let price: String                                        // 코인 현재가
-  let price_change_percentage_24h: TrendCoinPriceChangeDTO // 국가별 24시간 코인 변동률
   
   static var defaultValue: TrendCoinPriceDTO {
     
-    return TrendCoinPriceDTO(
-      price: .defaultValue,
-      price_change_percentage_24h: .defaultValue
-    )
-  }
-}
-
-struct TrendCoinPriceChangeDTO: DefaultValueProvidable {
-  
-  let krw: Double // 한국 코인 24시간 변동률
-  
-  static var defaultValue: TrendCoinPriceChangeDTO {
-    
-    return TrendCoinPriceChangeDTO(krw: .defaultValue)
+    return TrendCoinPriceDTO(price: .defaultValue)
   }
 }
