d8912926edc12304cf7164004cec8df69247a0bf
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 14:30:42 +0900

    Initial Commit

---
+++ KazCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KazCoin
+//
+//  Created by 원태영 on 2/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
