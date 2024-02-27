6dc37952e41621af66c761de2130e90ed6966dee
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 15:01:53 +0900

    [Build] 디렉토리 레이어 분리, 코드베이스 전환

---
+++ KazCoin/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  KazCoin
+//
+//  Created by 원태영 on 2/27/24.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let scene = (scene as? UIWindowScene) else { return }
+    
+    window = UIWindow(windowScene: scene)
+    window?.rootViewController = ViewController()
+    window?.makeKeyAndVisible()
+  }
+}
