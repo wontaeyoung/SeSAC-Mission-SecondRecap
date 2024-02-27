071f04766af18eeeff02e6fac31ce4eb7fa9869d
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 15:51:47 +0900

    [Add] CoinDesignSystem 프레임워크 타겟 추가

---
+++ CoinDesignSystem/CoinDesignSystem.h
@@ -0,0 +1,18 @@
+//
+//  CoinDesignSystem.h
+//  CoinDesignSystem
+//
+//  Created by 원태영 on 2/27/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoinDesignSystem.
+FOUNDATION_EXPORT double CoinDesignSystemVersionNumber;
+
+//! Project version string for CoinDesignSystem.
+FOUNDATION_EXPORT const unsigned char CoinDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoinDesignSystem/PublicHeader.h>
+
+
