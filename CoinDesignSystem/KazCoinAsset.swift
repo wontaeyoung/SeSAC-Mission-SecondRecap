5b09d9370488db1343c4bfd89976fa7f49903136
Taeyoung Won <hexter1994@gmail.com>
2024-02-27 15:52:21 +0900

    [Style] Coin 프로젝트 에셋 컬러 세팅

---
+++ CoinDesignSystem/KazCoinAsset.swift
@@ -0,0 +1,26 @@
+//
+//  KazCoinAsset.swift
+//  CoinDesignSystem
+//
+//  Created by 원태영 on 2/27/24.
+//
+
+import UIKit
+
+enum KazCoinAsset {
+  
+  enum Color {
+    
+    static let brand: UIColor = .init(hex: "914CF5")
+    static let minusLabel: UIColor = .init(hex: "F04452")
+    static let minusBackground: UIColor = .init(hex: "FFEAED")
+    static let plusLabel: UIColor = .init(hex: "3282F8")
+    static let plusBackground: UIColor = .init(hex: "E5F0FF")
+    static let titleName: UIColor = .init(hex: "000000")
+    static let searchCoinName: UIColor = .init(hex: "000000")
+    static let symbolName: UIColor = .init(hex: "828282")
+    static let primaryText: UIColor = .init(hex: "343D4C")
+    static let cardBackground: UIColor = .init(hex: "F3F4F6")
+    static let background: UIColor = .init(hex: "FFFFFF")
+  }
+}
