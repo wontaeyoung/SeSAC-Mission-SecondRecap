c24dc3d0ff3715764d1bbfe2cd1d567c8c5ccb1f
Taeyoung Won <hexter1994@gmail.com>
2024-02-28 18:12:50 +0900

    [Fix] 접근제어자 수정

--- CoinDesignSystem/KazCoinAsset.swift
+++ CoinDesignSystem/KazCoinAsset.swift
@@ -7,20 +7,20 @@
 
 import UIKit
 
-enum KazCoinAsset {
+public enum KazCoinAsset {
   
-  enum Color {
+  public enum Color {
     
-    static let brand: UIColor = .init(hex: "914CF5")
-    static let minusLabel: UIColor = .init(hex: "F04452")
-    static let minusBackground: UIColor = .init(hex: "FFEAED")
-    static let plusLabel: UIColor = .init(hex: "3282F8")
-    static let plusBackground: UIColor = .init(hex: "E5F0FF")
-    static let titleName: UIColor = .init(hex: "000000")
-    static let searchCoinName: UIColor = .init(hex: "000000")
-    static let symbolName: UIColor = .init(hex: "828282")
-    static let primaryText: UIColor = .init(hex: "343D4C")
-    static let cardBackground: UIColor = .init(hex: "F3F4F6")
-    static let background: UIColor = .init(hex: "FFFFFF")
+    public static let brand: UIColor = .init(hex: "914CF5")
+    public static let minusLabel: UIColor = .init(hex: "F04452")
+    public static let minusBackground: UIColor = .init(hex: "FFEAED")
+    public static let plusLabel: UIColor = .init(hex: "3282F8")
+    public static let plusBackground: UIColor = .init(hex: "E5F0FF")
+    public static let titleName: UIColor = .init(hex: "000000")
+    public static let searchCoinName: UIColor = .init(hex: "000000")
+    public static let symbolName: UIColor = .init(hex: "828282")
+    public static let primaryText: UIColor = .init(hex: "343D4C")
+    public static let cardBackground: UIColor = .init(hex: "F3F4F6")
+    public static let background: UIColor = .init(hex: "FFFFFF")
   }
 }
