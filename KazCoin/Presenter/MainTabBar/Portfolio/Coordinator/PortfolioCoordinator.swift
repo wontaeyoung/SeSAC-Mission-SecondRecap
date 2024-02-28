5cb8e3fbd539d62c21cdf433869fbfa3115e02eb
Taeyoung Won <hexter1994@gmail.com>
2024-02-28 18:15:39 +0900

    [Feat] 탭별 코디네이터 초기 세팅

---
+++ KazCoin/Presenter/MainTabBar/Portfolio/Coordinator/PortfolioCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  PortfolioCoordinator.swift
+//  KazCoin
+//
+//  Created by 원태영 on 2/28/24.
+//
+
+import UIKit
+import KazUtility
+
+final class PortfolioCoordinator: Coordinator {
+  
+  // MARK: - Property
+  weak var delegate: CoordinatorDelegate?
+  var navigationController: UINavigationController
+  var childCoordinators: [Coordinator]
+  
+  // MARK: - Initializer
+  init(_ navigationController: UINavigationController) {
+    self.navigationController = navigationController
+    self.childCoordinators = []
+  }
+  
+  // MARK: - Method
+  func start() {
+    
+  }
+}
+
---
+++ KazCoin/Presenter/MainTabBar/User/Coordinator/UserCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  UserCoordinator.swift
+//  KazCoin
+//
+//  Created by 원태영 on 2/28/24.
+//
+
+import UIKit
+import KazUtility
+
+final class UserCoordinator: Coordinator {
+  
+  // MARK: - Property
+  weak var delegate: CoordinatorDelegate?
+  var navigationController: UINavigationController
+  var childCoordinators: [Coordinator]
+  
+  // MARK: - Initializer
+  init(_ navigationController: UINavigationController) {
+    self.navigationController = navigationController
+    self.childCoordinators = []
+  }
+  
+  // MARK: - Method
+  func start() {
+    
+  }
+}
+
