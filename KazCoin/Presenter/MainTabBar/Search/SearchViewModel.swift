177cdb2291f85f4c051d70e70ee20cfb73e14048
Taeyoung Won <hexter1994@gmail.com>
2024-02-28 20:53:12 +0900

    [Feat] 즐겨찾기 관리 로직 연결

--- KazCoin/Presenter/MainTabBar/Search/SearchViewModel.swift
+++ KazCoin/Presenter/MainTabBar/Search/SearchViewModel.swift
@@ -7,16 +7,20 @@
 
 import Foundation
 import KazUtility
+import KazRealm
 
 final class SearchViewModel: ViewModel {
   
   // MARK: - I/O
   struct Input {
+    var viewDidLoadEvent: Observable<Void?> = .init(nil)
     var searchButtonTapEvent: Observable<String?> = .init(nil)
+    var interestButtonTapEvent: Observable<IndexPath?> = .init(nil)
   }
   
   struct Output {
-    var coins: Observable<[Coin]> = Observable([])
+    var coins: Observable<[Coin]> = .init([])
+    var interestCoins: Observable<[String]> = .init([])
   }
   
   var input = Input()
@@ -24,31 +28,60 @@ final class SearchViewModel: ViewModel {
   
   // MARK: - Property
   weak var coordinator: SearchCoordinator?
-  private let repository: any CoinRepository
+  private let coinRepository: any CoinRepository
+  private let interestRepository: any InterestRepository
   
   // MARK: - Initializer
-  init(repository: any CoinRepository) {
-    self.repository = repository
+  init(coinRepository: any CoinRepository, interestRepository: any InterestRepository) {
+    self.coinRepository = coinRepository
+    self.interestRepository = interestRepository
     
     transform()
   }
   
   // MARK: - Method
   func transform() {
-    input.searchButtonTapEvent.subscribe { text in 
+    input.viewDidLoadEvent.subscribe { [weak self] _ in
+      guard let self else { return }
+      
+      let interestCoins = interestRepository.fetch()
+      output.interestCoins.onNext(interestCoins)
+    }
+    
+    input.searchButtonTapEvent.subscribe { text in
       guard let text else { return }
       
       Task { [weak self] in
         guard let self else { return }
         
         do {
-          let coins = try await repository.fetch(by: text)
+          let coins = try await coinRepository.fetch(by: text)
           output.coins.onNext(coins)
         } catch {
-          guard let error = error as? AppError else { return }
           LogManager.shared.log(with: error, to: .network)
           coordinator?.showErrorAlert(error: error)
         }
+      }
+    }
+    
+    input.interestButtonTapEvent.subscribe { [weak self] indexPath in
+      guard let self else { return }
+      guard let indexPath else { return }
+      let selectedCoin: Coin = output.coins.current[indexPath.row]
+      
+      do {
+        guard let index = output.interestCoins.current.firstIndex(of: selectedCoin.id) else {
+          try interestRepository.create(with: selectedCoin)
+          output.interestCoins.value.append(selectedCoin.id)
+          return
+        }
+        
+        try interestRepository.delete(with: selectedCoin)
+        output.interestCoins.value.remove(at: index)
+        
+      } catch {
+        LogManager.shared.log(with: error, to: .local)
+        coordinator?.showErrorAlert(error: error)
       }
     }
   }
@@ -59,5 +92,9 @@ final class SearchViewModel: ViewModel {
   
   func itemAt(_ indexPath: IndexPath) -> Coin {
     return output.coins.current[indexPath.row]
+  }
+  
+  func interestedAt(_ indexPath: IndexPath) -> Bool {
+    return output.interestCoins.current.contains(itemAt(indexPath).id)
   }
 }
