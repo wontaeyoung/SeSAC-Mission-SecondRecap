3fce83870327b83186eb35c1827067b47a1e68b7
Taeyoung Won <hexter1994@gmail.com>
2024-02-29 15:02:20 +0900

    [Fix] 색상 이름 오류 수정

--- CoinDesignSystem/KazCoinAsset.swift
+++ CoinDesignSystem/KazCoinAsset.swift
@@ -12,10 +12,10 @@ public enum KazCoinAsset {
   public enum Color {
     
     public static let brand: UIColor = .init(hex: "914CF5")
-    public static let minusLabel: UIColor = .init(hex: "F04452")
-    public static let minusBackground: UIColor = .init(hex: "FFEAED")
-    public static let plusLabel: UIColor = .init(hex: "3282F8")
-    public static let plusBackground: UIColor = .init(hex: "E5F0FF")
+    public static let plusLabel: UIColor = .init(hex: "F04452")
+    public static let plusBackground: UIColor = .init(hex: "FFEAED")
+    public static let minusLabel: UIColor = .init(hex: "3282F8")
+    public static let minusBackground: UIColor = .init(hex: "E5F0FF")
     public static let titleName: UIColor = .init(hex: "000000")
     public static let searchCoinName: UIColor = .init(hex: "000000")
     public static let symbolName: UIColor = .init(hex: "828282")
