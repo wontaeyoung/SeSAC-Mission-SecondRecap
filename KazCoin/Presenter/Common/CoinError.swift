56f1d54208bd917e57b9fb37a124eafab64fab0a
Taeyoung Won <hexter1994@gmail.com>
2024-02-29 21:08:32 +0900

    [Add] 코인 조회 결과 없음 에러 추가

--- KazCoin/Presenter/Common/CoinError.swift
+++ KazCoin/Presenter/Common/CoinError.swift
@@ -10,11 +10,15 @@ import KazUtility
 enum CoinError: AppError {
   
   case cannotOverMaximumInterest(max: Int)
+  case noResultWithID(id: String)
   
   var logDescription: String {
     switch self {
       case .cannotOverMaximumInterest:
         return "관심 코인 최대 갯수 초과"
+        
+      case .noResultWithID(let id):
+        return "\(id) 코인 마켓 조회 결과 없음"
     }
   }
   
@@ -22,6 +26,9 @@ enum CoinError: AppError {
     switch self {
       case .cannotOverMaximumInterest(let max):
         return "관심 코인은 \(max)개까지만 설정 가능해요!"
+        
+      case .noResultWithID:
+        return "코인 정보를 찾을 수 없어요. 확인을 누르면 이전 화면으로 돌아가요."
     }
   }
 }
