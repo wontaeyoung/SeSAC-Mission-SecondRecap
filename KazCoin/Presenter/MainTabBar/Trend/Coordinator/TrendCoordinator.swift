f823305495b14de4655fabf6a13e5118438603f2
Taeyoung Won <hexter1994@gmail.com>
2024-03-03 20:04:59 +0900

    [Feat] 트렌드 화면 코디네이터 연결

--- KazCoin/Presenter/MainTabBar/Trend/Coordinator/TrendCoordinator.swift
+++ KazCoin/Presenter/MainTabBar/Trend/Coordinator/TrendCoordinator.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import KazUtility
+import KazRealm
 
 final class TrendCoordinator: Coordinator {
   
@@ -23,6 +24,20 @@ final class TrendCoordinator: Coordinator {
   
   // MARK: - Method
   func start() {
+    showTrendView()
+  }
+  
+  func showTrendView() {
+    let service = LiveRealmService()
+    let interestRepository = LiveInterestRepository(service: service)
+    let coinRepository = LiveCoinRepository()
+    let trendRepository = LiveTrendRepository()
+    let viewModel = TrendViewModel(interestRepository: interestRepository, coinRepository: coinRepository, trendRepository: trendRepository)
+    let viewController = TrendViewController(viewModel: viewModel)
+      .navigationTitle(with: MainTabBarPage.trend.navigationTitle, displayMode: .always)
+      .hideBackTitle()
     
+    viewModel.coordinator = self
+    self.push(viewController)
   }
 }
