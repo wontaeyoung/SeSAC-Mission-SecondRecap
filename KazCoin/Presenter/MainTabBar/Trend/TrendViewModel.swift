8534e6fccd07e918e861405603d8f7b6643fa291
Taeyoung Won <hexter1994@gmail.com>
2024-03-03 20:04:50 +0900

    [Feat] 즐겨찾기 코인 조회 로직 구현

--- KazCoin/Presenter/MainTabBar/Trend/TrendViewModel.swift
+++ KazCoin/Presenter/MainTabBar/Trend/TrendViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 원태영 on 3/2/24.
 //
 
+import Foundation
 import KazUtility
 
 final class TrendViewModel: ViewModel {
@@ -20,23 +21,50 @@ final class TrendViewModel: ViewModel {
   }
   
   struct Input {
-    
+    var viewDidLoadEvent: Observable<Void?> = .init(nil)
+    var viewWillAppearEvent: Observable<Void?> = .init(nil)
   }
   
   struct Output {
-    
+    var interestCoins: Observable<[Coin]> = .init([])
+    var topCoins: Observable<[Coin]> = .init([])
+    var topNFT: Observable<[NFT]> = .init([])
+    var loadingIndicatorToggle: Observable<Bool?> = .init(nil)
+    var updateFavoriteSection: Observable<Void?> = .init(nil)
   }
+  
+  var input = Input()
+  var output = Output()
   
   // MARK: - Property
   weak var coordinator: TrendCoordinator?
-  private let repository: any TrendRepository
-  var sections: [TrendSection] {
+  private let interestRepository: any InterestRepository
+  private let coinRepository: any CoinRepository
+  private let trendRepository: any TrendRepository
+  private var currentInterestCoins: [String] = []
+  
+  private var sections: [TrendSection] {
     return TrendSection.allCases
   }
   
+  private var shouldFavoriteSectionHidden: Bool {
+    let shouldShowFavoriteCoinCount: Int = 2
+    return output.interestCoins.current.count < shouldShowFavoriteCoinCount
+  }
+  
+  private var favoriteSectionHiddenBuffer: Int {
+    return shouldFavoriteSectionHidden ? 1 : 0
+  }
+  
   // MARK: - Initializer
-  init(repository: any TrendRepository) {
-    self.repository = repository
+  init(
+    interestRepository: any InterestRepository,
+    coinRepository: any CoinRepository,
+    trendRepository: any TrendRepository
+  ) {
+    self.interestRepository = interestRepository
+    self.coinRepository = coinRepository
+    self.trendRepository = trendRepository
     
     transform()
   }
@@ -44,14 +72,84 @@ final class TrendViewModel: ViewModel {
   // MARK: - Method
   func transform() {
     
+    input.viewDidLoadEvent.subscribe { [weak self] _ in
+      guard let self else { return }
+      currentInterestCoins = interestRepository.fetch()
+      
+      Task { [weak self] in
+        guard let self else { return }
+        
+        output.loadingIndicatorToggle.onNext(true)
+        defer { output.loadingIndicatorToggle.onNext(false) }
+        
+        do {
+          let coins = try await coinRepository.fetch(from: currentInterestCoins)
+          output.interestCoins.onNext(coins)
+        } catch {
+          LogManager.shared.log(with: error.localizedDescription, to: .network, level: .debug)
+          LogManager.shared.log(with: error, to: .network)
+          coordinator?.showErrorAlert(error: error)
+        }
+      }
+    }
+    
+    input.viewWillAppearEvent.subscribe { [weak self] _ in
+      guard let self else { return }
+      let newInterestCoins = interestRepository.fetch()
+      let isInterestChagned: Bool = currentInterestCoins != newInterestCoins
+      
+      guard isInterestChagned else { return }
+      currentInterestCoins = newInterestCoins
+      
+      Task { [weak self] in
+        guard let self else { return }
+        
+        output.loadingIndicatorToggle.onNext(true)
+        defer { output.loadingIndicatorToggle.onNext(false) }
+        
+        do {
+          let coins = try await coinRepository.fetch(from: currentInterestCoins)
+          output.interestCoins.onNext(coins)
+        } catch {
+          LogManager.shared.log(with: error.localizedDescription, to: .network, level: .debug)
+          LogManager.shared.log(with: error, to: .network)
+          coordinator?.showErrorAlert(error: error)
+        }
+      }
+    }
   }
   
   func numberOfSection() -> Int {
-    return sections.count
+    return sections.count - favoriteSectionHiddenBuffer
   }
   
   func sectionAt(_ index: Int) -> TrendSection {
-    return sections[index]
+    return sections[index + favoriteSectionHiddenBuffer]
+  }
+  
+  func sectionAt(_ indexPath: IndexPath) -> TrendSection {
+    return sections[indexPath.section + favoriteSectionHiddenBuffer]
+  }
+  
+  func numberOfItems(_ section: Int) -> Int {
+    switch sectionAt(section) {
+      case .interest:
+        return output.interestCoins.current.count
+      case .topCoin:
+        return output.topCoins.current.count
+      case .topNFT:
+        return output.topNFT.current.count
+    }
+  }
+  
+  func itemAt(_ indexPath: IndexPath) -> any Entity {
+    switch sectionAt(indexPath) {
+      case .interest:
+        return output.interestCoins.current[indexPath.row]
+      case .topCoin:
+        return output.topCoins.current[indexPath.row]
+      case .topNFT:
+        return output.topNFT.current[indexPath.row]
+    }
   }
 }
-
